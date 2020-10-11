21ecb2dc7fa492632f54716fad8734e9e69aadfc
Brian Izaya <alexanderizaya@gmail.com>
2020-10-11 05:28:03 -0300

    news endpount

--- src/app.ts
+++ src/app.ts
@@ -4,5 +4,13 @@ import { PORT } from "./appsetting";
 server.listen(PORT) 
 
 server.get("/",(request, response) => {
-    response.send("Buena parse por 3");
+    response.send("Hello there");
+});
+
+server.get("/user",(request, response) => {
+    response.send({name: "Kraken" , lastName: "The best"});
+});
+
+server.get("/app",(request, response) => {
+    response.send({title:"New dashboard APP", body:"Exit dashboard app"});
 });
